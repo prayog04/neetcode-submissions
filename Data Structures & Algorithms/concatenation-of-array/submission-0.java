60338d7bb5c099480917b50327254fbcb4778740
Praveen  Nagaraju <praveen.prayaag@gmail.com>
2026-08-01 11:43:17 -0700

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int[] arr = new int[2*len];
+        int newInd = len;
+        for(int i=0;i<len;i++){
+            arr[i] = nums[i];
+            arr[len + i] = nums[i]; 
+        }
+
+        return arr;
+    }
+}
